13632c6db8449d67997f2995fc70d72b6e51f0f5
swback <1760349255@qq.com>
2024-06-22 23:00:31 +0800

    增加油猴脚本

--- 形势与政策-自动匹配答案.user.js
+++ 形势与政策-自动匹配答案.user.js
@@ -1,65 +1,3 @@
-形势与政策与马克思题目在线查询网站
-=======
-> **系统未做安全性保护,强烈建议以docker形式部署。**
->
-> 完美兼容 南京工业职业技术大学 马克思主义 和 形势与政策 两门课程考试题目。
-
-# 系统功能
-
-在线登录
-
-后台管理
-
-题目查询
-
-油猴插件
-
-# 后台账号
-
-| 账户  | 密码   | 权限       |
-| ----- | ------ | ---------- |
-| admin | swback | 超级管理员 |
-| test  | swback | 普通用户   |
-
-# 部署环境
-
-## 方式一: 直接部署
-
-推荐: `nginx1.18` + `php72`+`MySQL5.6`
-
-## 方式二: Docker拉取-强烈推荐
-
-制作中
-
-# 数据配置
-
-## 题库切换
-
-数据库中存在`马克思` and `形式与政策` 两个题库，如果需要切换题库,请修改`function.php` 第36行,将`analyze` 修改为`marks`即可
-
-![image-20240620225105033](https://gitee.com/swback/typora/raw/master/img/202406202302872.png)
-
-## 数据库配置
-
-修改config.php文件,填写数据库账户，密码,数据库名
-
-![image-20240620225154629](https://gitee.com/swback/typora/raw/master/img/202406202303291.png)
-
-# 系统截图
-
-![image-20240620222644341](https://gitee.com/swback/typora/raw/master/img/202406202302364.png)
-
-![image-20240620222726947](https://gitee.com/swback/typora/raw/master/img/202406202303232.png)
-
-![image-20240620222804958](https://gitee.com/swback/typora/raw/master/img/202406202303478.png)
-
-![image-20240620222830944](https://gitee.com/swback/typora/raw/master/img/202406202303344.png)
-
-![image-20240620222916668](https://gitee.com/swback/typora/raw/master/img/202406202303693.png)
-
-# 油猴脚本
-
-```javascript
 // ==UserScript==
 // @name         形势与政策-自动匹配答案
 // @namespace    http://tampermonkey.net/
@@ -337,6 +275,3 @@
 
 
 })();
-
-```
-
